c0ae0cb513f292f34a3a88c94a5f24e94eeb558c
Thomas Schofield <twschofield99@gmail.com>
2019-02-17 19:28:21 -0500

    Add more detailed testing for the ROM reading
    Read 8 bytes at a time, then read the first 8 bytes in 1 byte steps.

--- ARM_64/memory/ROM/ROM_64bit_testbench.v
+++ ARM_64/memory/ROM/ROM_64bit_testbench.v
@@ -5,24 +5,52 @@ module ROM_64bit_testbench();
 	reg clock;
 	
 	ROM_64bit dut (data, address, clock, size);
-
-	// defparam dut.ADDR_WIDTH = 11;
-
-	// wire [7:0] addr;
-	// assign addr = address;
 	
 	initial begin
 		clock <= 1'b0;
 		address <= 8'b0;
-		size <= 2'b11;
-		#640 $stop;
+		size <= 2'b11; // Initially complete 8 byte memory reads
+		#1280 $stop;
 	end
 	
 	always
 		#5 clock <= ~clock;
-	
-	always
-		#10 address <= address + 4'b1000;
+
+
+	always begin
+		repeat(32) begin
+			#10 address <= address + 4'b1000;
+		end
+
+		#5
+		size <= 2'b00;
+		address <= 8'b00000000;
+
+		#5
+		repeat (7) begin
+			#10 address <= address + 4'b0001;
+		end
+
+		#10
+		repeat(32) begin
+			#10 address <= address + 4'b1000;
+		end
+
+		#5
+		size <= 2'b01;
+		address <= 8'b00000000;
+
+		#5
+		repeat (4) begin
+			#10 address <= address + 4'b0001;
+		end
+
+	end
+
+
+	// always begin
+		
+	// end
 
 	// always 
 	// 	#160 size <= size + 1'b1;
