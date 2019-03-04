3855d0e9ff94295d7e77f5c5e6d5d077227e3ab3
Thomas Schofield <thomas.schofield@ieee.org>
2019-03-03 19:38:48 -0500

    Minor changes to datapath and testbench

--- ARM_64/Datapath/Datapath_With_Memory_LEGv8.v
+++ ARM_64/Datapath/Datapath_With_Memory_LEGv8.v
@@ -1,4 +1,4 @@
-module Datapath_With_Memory_LEGv8(ControlWord, data, address, reset, clock, constant, status, write_enable, read_enable, size, r0, r1, r2, r3, r4, r5, r6, r7);
+module Datapath_With_Memory_LEGv8(ControlWord, data, address, reset, clock, constant, status, r0, r1, r2, r3, r4, r5, r6, r7); // before r0 -> write_enable, read_enable, size,
 	//                         Datapath_LEGv8 (data, address, reset, clock, constant, status, FS, C0, Bsel, EN_ALU, EN_B, EN_ADDR_ALU, r0, r1, r2, r3, r4, r5, r6, r7);
 	
 	input [31:0] ControlWord; // Combination of control signals
@@ -17,8 +17,8 @@ module Datapath_With_Memory_LEGv8(ControlWord, data, address, reset, clock, cons
 	output [3:0] status; // V - Overflow detection 1 (yes) / 0 (no), C - carry bit, N - Sign bit, Z - 1 (ALU output is zero) / 0 (ALU output isn't zero)
 
 	// Memory inputs
-	input write_enable, read_enable;
-	input [1:0] size; // transfer size: 00 8-bit, 01 16-bit, 10 32-bit, 11 64-bit
+	// input write_enable, read_enable;
+	// input [1:0] size; // transfer size: 00 8-bit, 01 16-bit, 10 32-bit, 11 64-bit
 
 	// Visualization outputs
 	output [15:0] r0, r1, r2, r3, r4, r5, r6, r7;
@@ -33,15 +33,14 @@ module Datapath_With_Memory_LEGv8(ControlWord, data, address, reset, clock, cons
 	wire C0;
 
 	// RAM
-	wire chip_select;
+	wire chip_select, write_enable, read_enable;
+	wire [1:0] size;
 	//input chip_select;
 
 	// Tristate signals
 	wire EN_ALU;
 	wire EN_B;
 	wire EN_ADDR_ALU;
-
-	wire [4:0] junk_values;
 
 	// Assign Values from ControlWord
 	// 32'b0_00_1_0_1_0_00_1_00_00100_00000_00000_00000;
@@ -63,12 +62,16 @@ module Datapath_With_Memory_LEGv8(ControlWord, data, address, reset, clock, cons
 	assign EN_ALU = ControlWord [24];
 	assign EN_B = ControlWord [25];
 	assign EN_ADDR_ALU = ControlWord [26];
-	assign junk_values = ControlWord [31:27];
+	assign size = ControlWord [28:27];
+	assign write_enable = ControlWord [29];
+	assign read_enable = ControlWord [30];
 
 	//            Datapath_LEGv8 (data, address, reset, clock, constant, DA, SA, SB, W, status, FS, C0, Bsel, EN_ALU, EN_B, EN_ADDR_ALU, r0, r1, r2, r3, r4, r5, r6, r7);
 	Datapath_LEGv8 base_datapath (data, address, reset, clock, constant, DA, SA, SB, W, status, FS, C0, Bsel, EN_ALU, EN_B, EN_ADDR_ALU, r0, r1, r2, r3, r4, r5, r6, r7);
 	
+	//AddressDetect(address, out);
+
 	//   RAM_64bit(clock, address, data, chip_select, write_enable, output_enable, size);
 	RAM_64bit ram (clock, address, data, chip_select, write_enable, read_enable, size);
-	
+	defparam ram.ADDR_WIDTH = 16;
 endmodule
