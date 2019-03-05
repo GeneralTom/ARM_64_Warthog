02e8d385ad0afad21a0ec53d9daac6c075f9f48b
Thomas Schofield <twschofield99@gmail.com>
2019-03-04 21:19:42 -0500

    Add ControlWords for memory write in testbench

--- ARM_64/Datapath/Datapath_With_Memory_testbench.v
+++ ARM_64/Datapath/Datapath_With_Memory_testbench.v
@@ -15,7 +15,8 @@ module Datapath_With_Memory_testbench();
 
 	wire [15:0] r0, r1, r2, r3, r4, r5, r6, r7;
 
-	Datapath_With_Memory_LEGv8 dut (ControlWord, data, address, reset, clock, constant, status, mem_write, mem_read, size, r0, r1, r2, r3, r4, r5, r6, r7);
+	Datapath_With_Memory_LEGv8 dut (ControlWord, data, address, reset, clock, constant, status, r0, r1, r2, r3, r4, r5, r6, r7);
+	// mem_write, mem_read, size,
 
 	initial begin
 		clock <= 1'b0;
@@ -29,29 +30,24 @@ module Datapath_With_Memory_testbench();
 
 	always begin
 		#5 reset <= 1'b0;
-
-		// #5
-		// ControlWord <= 32'b00000_0_0_1_0_0_00100_1_1_00001_11111_00000;
-		// #10
 		
-		// R0 <-- R31 | Constant
+		// R0 <-- R31 | Constant (Working, don't touch!)
 		#5
 		ControlWord <= 32'b00000_0_0_1_0_0_00100_1_1_00001_11111_00000;
 		#10
 
-        // R1 <-- R31 - R0
+        // R1 <-- R31 - R0 (Working, don't touch!)
         ControlWord <= 32'b00000_0_0_1_0_1_01001_0_1_00000_11111_00001;
         #10
 
         // M[R31 + constant] <-- R1
         ControlWord <= 32'b00000_0_0_1_0_0_00100_1_1_00000_11111_00111;
         #10
-        ControlWord <= 32'b00000_0_0_1_0_0_00100_1_1_00000_11111_00111;
-        //ControlWord <= 32'b00000_1_0_1_0_1_01001_1_1_00000_11111_00001;
-        
+        ControlWord <= 32'b00111_1_1_0_1_0_00100_0_0_00111_00001_00000;
+        #10
 
-        // R1 <-- R0 & R1
-        ControlWord <= 32'b00000_0_1_0_0_0_00000_0_1_00001_00000_00001;
+        // R1 <-- R0 & R1 (Working, don't touch!)
+        ControlWord <= 32'b00000_0_0_1_0_0_00000_0_1_00001_00000_00001;
         #10
 
         // R2 <-- M[R31 + constant]
