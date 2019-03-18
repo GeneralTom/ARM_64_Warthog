d0ec7f1a7ab32802be4a491791d322f03f9e17b5
Thomas Schofield <twschofield99@gmail.com>
2019-03-18 13:12:15 -0400

    Add base address parameter, and organize file

--- ARM_64/memory/RAM/RAM_Detect.v
+++ ARM_64/memory/RAM/RAM_Detect.v
@@ -1,16 +1,20 @@
-module RAM_Detect(clock, address, data, chip_select, write_enable, output_enable, size);
-
+module RAM_Detect(data, address, mem_write, mem_read, size, clock);
+	parameter BASE_ADDR = 32'h00000000;
 	parameter ADDR_WIDTH = 8;
-	input clock;
-	input [ADDR_WIDTH-1:0] address;
+
 	inout [63:0] data;
-	input chip_select, write_enable, output_enable;
-	input [1:0] size;
+	input [31:0] address;
+	input mem_write, mem_read;
+	input [1:0] size; // 00 - 8-bit, 01 - 16-bit, 10 - 32-bit, 11 - 64-bit
+	input clock;
 
-	wire out;
+	wire chip_select;
 
-	AddressDetect detRAM(address, out);
-	RAM_64bit detRAM(clock, address, data, out, write_enable, output_enable, size);
+	AddressDetect detRAM(address, chip_select);
+	defparam detRAM.base_address = BASE_ADDR;
+	defparam detRAM.address_mask = 32'hFFFFFFFF << ADDR_WIDTH;
 
+	RAM_64bit detRAM(clock, address, data, chip_select, write_enable, output_enable, size);
+	defparam detRAM.ADDR_WIDTH = ADDR_WIDTH;
 endmodule
 
