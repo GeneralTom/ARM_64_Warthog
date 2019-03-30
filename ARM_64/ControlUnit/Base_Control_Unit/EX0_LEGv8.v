3f3c7161649bc9476083591016593d20b68e7353
Thomas Schofield <schofielt7@students.rowan.edu>
2019-03-30 17:21:21 -0400

    Add select bit logic to EX0, and add blank file
    The blank file will be for the Data Immediate ControlWords.

--- ARM_64/ControlUnit/Base_Control_Unit/EX0_LEGv8.v
+++ ARM_64/ControlUnit/Base_Control_Unit/EX0_LEGv8.v
@@ -11,26 +11,41 @@
 // the specific inputs from the mux.
 /////////////////////////////////////////////////////////
 
-// TODO: Finish Add combinational logic to convert 
-//       instruction_bits to select bits
+module EX0_LEGv8 (ControlWord, instruction, data_imm, branch, mem, data_reg);
+	parameter CONTROL_N = 40;
+	parameter INSTRUCTION_N = 32;
 
-module EX0_LEGv8 (ControlWord, instruction_bits, data_imm, branch, mem, data_reg);
-	output [39:0] ControlWord;
+	output [CONTROL_N-1:0] ControlWord;
 
-	input [3:0] instruction_bits;
-	input [39:0] data_imm;
-	input [39:0] branch;
-	input [39:0] mem;
-	input [39:0] data_reg;
+	input [INSTRUCTION_N-1:0] instruction_bits;
+	input [CONTROL_N-1:0] data_imm;
+	input [CONTROL_N-1:0] branch;
+	input [CONTROL_N-1:0] mem;
+	input [CONTROL_N-1:0] data_reg;
+
+	wire [1:0] select;
+
+	E1 encoder1 (select, instruction);
+	defparam encoder1.N = INSTRUCTION_N;
 
 	// Mux4to1Nbit(F, S, I0, I1, I2, I3)
-	Mux4to1Nbit EX0_Mux (ControlWord, , data_imm, branch, mem, data_imm);
+	Mux4to1Nbit EX0_Mux (ControlWord, select, data_imm, branch, mem, data_imm);
+	defparam EX0_Mux.N = CONTROL_N;
 endmodule
 
 // E1 is the combinational logic that uses the bits
 // from instruction_bits to create select bits for
 // the 4:1 mux
 
-module E1 ();
+module E1 (out, instruction);
+	parameter N = 32;
+	output [1:0] out;
+	input [N-1:0] instruction;
 
+	wire S0, S1;
+
+	assign S0 = (I[27] & ~I[26] & I[25]) | (I[28] & ~I[27] & I[26]);
+	assign S1 = I[27] & (~I[26] | I[25]);
+
+	assign out = {S1, S0};
 endmodule
