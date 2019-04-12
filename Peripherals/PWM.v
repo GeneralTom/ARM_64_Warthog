55c73e4a7822c7b7f1e644ac767cca6140eabd57
Thomas Schofield <twschofield99@gmail.com>
2019-04-12 19:26:13 -0400

    Add empty PWM file

---
+++ Peripherals/PWM.v
@@ -0,0 +1,13 @@
+module PWM ();
+    parameter base_address = 32'h80000000;
+
+endmodule
+
+module Timer_64 ();
+
+endmodule
+
+module Counter_64 (clock, reset, );
+    input clock, reset
+    
+endmodule
