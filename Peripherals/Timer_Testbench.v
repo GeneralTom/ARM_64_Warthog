b21208a8a3d438e93e9db754c9bf5ade66ad50c2
Thomas Schofield <twschofield99@gmail.com>
2019-04-22 20:14:49 -0400

    Finish initial timer testing (separate testbench)

--- Peripherals/Timer_Testbench.v
+++ Peripherals/Timer_Testbench.v
@@ -11,10 +11,9 @@ module Timer_Testbench ();
     assign data_out = data_bi;
     assign data_bi = (mem_write == 1'b1) ? data_in : 64'bZ;
 
-    Timer dut (data_bi, address, mem_write, mem_read, size, clock, reset);
+    Timer_TS dut (data_bi, address, mem_write, mem_read, size, clock, reset);
     defparam dut.BASE_ADDR = 32'h80000000;
     defparam dut.ADDR_WIDTH = 8;
-    defparam dut.TIMER_SIZE = 32;
 
     initial begin
         address <= 64'b0;
@@ -30,14 +29,39 @@ module Timer_Testbench ();
         #5 clock <= ~clock;
 
     always begin
-        #5
-        data_in <= 64'h0000000000000001;
-        address <= 32'h80000000;
+        #10 // Sets period to 8'h20
+        data_in <= 64'h0000000000000020;
+        address <= 32'h80000002;
         mem_write <= 1'b1;
         mem_read <= 1'b0;
-        #10
-        mem_read <= 1'b1;
+        #10 // Read period back
         mem_write <= 1'b0;
-        #100 $stop;
+        mem_read <= 1'b1;
+        #10 // Set the conditions register to start the timer and use period
+        data_in <= 64'b0101;
+        address <= 32'h80000001;
+        mem_write <= 1'b1;
+        mem_read <= 1'b0;
+        #10 // Just view timer output
+        address <= 32'h80000000;
+        mem_write <= 1'b0;
+        mem_read <= 1'b1;
+        #500 // View the condition register
+        address <= 32'h80000001;
+        mem_write <= 1'b0;
+        mem_read <= 1'b1;
+        #10 // Set the condition register to not use the period
+        data_in <= 64'b0001;
+        address <= 32'h80000001;
+        mem_write <= 1'b1;
+        mem_read <= 1'b0;
+        #10 // Watch output again
+        address <= 32'h80000000;
+        mem_write <= 1'b0;
+        mem_read <= 1'b1;
+        #480 // Change address to test if timer base address detect is working
+        address <= 32'h70000000;
+        #50
+        $stop;
     end
 endmodule
